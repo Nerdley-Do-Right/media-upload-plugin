f1a45c80c86c36c7d1626104b772558e1d21390a
GrandguyJS <GrandguyMC@gmail.com>
2025-01-02 19:07:01 +0100

    1.1.9 update - CRUCIAL - Need Administrator Role to use API functions

--- Jellyfin.Plugin.RemoteUpload/Api/RemoteUploadController.cs
+++ Jellyfin.Plugin.RemoteUpload/Api/RemoteUploadController.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Jellyfin.Plugin.RemoteUpload.Api;
 
 [ApiController]
+[Authorize(Roles = "Administrator")]
 [Route("mediaupload")]
 public class UploadController : ControllerBase
 {
